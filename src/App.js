b4622e220a4f4292718147f048aa426270f7c31f
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-03-09 21:45:38 +0530

    index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+// import mygif from './mygif.gif';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-
+import myvideo from './myvideo.mp4';
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -10,6 +11,11 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 function App() {
   return (
+    
+    <div className="videodiv">
+      <video autoPlay loop muted id="myVideo" >
+        <source src={myvideo} type="video/mp4" />
+      </video>
     <div className="App">
      <div className="login_container">
      <Navbar expand="lg">
@@ -25,10 +31,21 @@ function App() {
   </Navbar.Collapse>
 </Navbar>
 <Row className="row">
-  <Col className="col-md-4"></Col>
-  <Col className="col-md-8"></Col>
+  <Col className="col-md-6">
+    <div className="c1">
+      <h1 className="text1">Connect.</h1>
+      <h1 className="text1">Grow.</h1>
+      <h1 className="text1">Inspire.</h1>
+      <p className="para1">Engage with your friends through a full feature chat integration.</p>
+      <Button className="btn btn-primary">Sign In</Button>
+    </div>
+  </Col>
+  {/* <Col className="col-md-6">
+    <img id="img1" src={mygif}></img>
+  </Col> */}
 </Row>
      </div>
+    </div>
     </div>
   );
 }
