701270c12a8c7e52ec5cfdbd7ebe98d427ead3cf
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-03-09 16:52:41 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
+
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="login_container">
+     <Navbar expand="lg">
+  <Navbar.Brand id="navlogo" href="#home">Duolouge</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link id="hlink" >About Us</Nav.Link>
+      <Button id="register" className="btn btn-primary">Sign Up</Button>
+     
+    </Nav>
+    
+  </Navbar.Collapse>
+</Navbar>
+<Row className="row">
+  <Col className="col-md-4"></Col>
+  <Col className="col-md-8"></Col>
+</Row>
+     </div>
     </div>
   );
 }
