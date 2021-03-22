ca6968bc0a4ac81dc6e5816b2ef6b2750e7ae8e5
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-03-22 18:24:55 +0530

    find users added

--- src/components/Finduser.jsx
+++ src/components/Finduser.jsx
@@ -1,15 +1,22 @@
 import React,{useState} from "react";
 import { Col,Row,Form,Button,FormControl } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {fas, faSearch} from '@fortawesome/free-solid-svg-icons'
 function Finduser()
 {
     return(
-        <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
+        <div className="searchUsers">
+        <h1>Users in Your Area</h1>
+        <Form className="row">
+            <Form.Group controlId="searchUser">
     
     <Form.Control type="text" placeholder="Search" />
   </Form.Group>
+  <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
         </Form>
+      
+        </div>
+
     );
 }
 export default Finduser;
